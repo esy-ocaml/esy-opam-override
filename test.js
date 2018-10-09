ff4977cc6eba909fb0f568a177e4eedd1cac041d
Bryan Phelps <bryphe@outlook.com>
2018-10-09 14:40:37 -0700

    Use top-level folder

--- test.js
+++ test.js
@@ -29,6 +29,12 @@ const getRelevantPackagesToTest = () => {
 }
 
 const mkdirTemp = (packageFolder) => {
+    // For AppVeyor, use a root-level folder - the permissions
+    // in the temp folder are problematic
+    const tempFolder = os.platform() === "win32" ? "C:/esy-temp" : os.tmpdir();
+    fs.mkdirSync(tempFolder);
+    console.log(" - Using temp folder: " + tempFolder);
+
     const p = path.join(os.tmpdir(), packageFolder + crypto.randomBytes(4).toString("hex")); 
     fs.mkdirSync(p);
     return p;
